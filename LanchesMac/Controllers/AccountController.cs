5382e7dcad5530d5cb2ebe0ca880fb2860dcb31c
Lênin Colares <lenincolares@hotmail.com>
2022-07-07 11:38:41 -0300

    Creating Register

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -48,5 +48,33 @@ namespace LanchesMac.Controllers
             return View(loginVM);
 
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroVM.UserName };
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+
+                }
+            }
+            return View(registroVM);
+        }
+
     }
 }
